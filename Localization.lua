35c767ee998727c626116dfba16e164218adda8d
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2025-12-19 13:37:08 +0800

    Update Localization.lua

--- Localization.lua
+++ Localization.lua
@@ -66,7 +66,7 @@ local L = {
         QUEST_ACCEPTED = "已自动接受任务。",
         QUEST_TURNIN = "已自动完成任务。",
         QUEST_PROGRESS = "目前已完成任务：|cnNORMAL_FONT_COLOR:%d|r/|cnNORMAL_FONT_COLOR:%d|r，宝藏位置：|cnNORMAL_FONT_COLOR:%s|r。",
-        WAYPOINT_SET = "已标记宝藏位置。",
+        WAYPOINT_SET = "已地图标记宝藏位置。",
     }
 };
 
